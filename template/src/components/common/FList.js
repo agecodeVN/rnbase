84d0c9283acca43c9ce74bc984030f2f6cc5f1d7
An Nguyễn Phước <31690470+phuocantd@users.noreply.github.com>
2022-06-13 16:24:08 +0700

    config interaction flist width height common (#5)
    * config interaction flist width height common
    
    * update

--- template/src/components/common/FList.js
+++ template/src/components/common/FList.js
@@ -1,11 +1,12 @@
 import React, { useRef } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
-import { Spacings, View, Text, Image } from 'react-native-ui-lib';
+import { Spacings, View, Text, Image, Colors } from 'react-native-ui-lib';
 import { useScrollToTop } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useRefreshByUser } from 'hooks';
 import { AfterInteraction } from 'components';
 import isEqual from 'react-fast-compare';
+import { UIActivityIndicator } from 'react-native-indicators';
 
 const FList = ({
   keyId = 'id',
@@ -15,18 +16,21 @@ const FList = ({
   loadMore,
   isFetching,
   hasNextPage,
+  isSuccess,
   renderItem,
   renderEmpty,
   renderFooter,
   renderHeader,
   contentContainerStyle,
   textEmpty = '',
+  showEmptyIcon = true,
   ...props
 }) => {
   const flatlistRef = useRef(null);
   const { bottom } = useSafeAreaInsets();
 
   const { isRefetchingByUser, refetchByUser } = useRefreshByUser(refetch);
+  const windowSize = data?.length > 50 ? data.length / 4 : 21;
 
   useScrollToTop(flatlistRef);
 
@@ -36,11 +40,25 @@ const FList = ({
     }
     return (
       <View flex center>
-        <Image alert assetName={'none_result'} assetGroup={'alert'} />
+        {showEmptyIcon && <Image empty assetName={'empty'} />}
         <Text marginV-xl bold>
           {textEmpty}
         </Text>
       </View>
+    );
+  };
+
+  const ListFooterComponent = () => {
+    if (renderFooter) {
+      return renderFooter;
+    }
+
+    return (
+      hasNextPage && (
+        <View paddingT-md>
+          <UIActivityIndicator color={Colors.primary} />
+        </View>
+      )
     );
   };
 
@@ -52,18 +70,20 @@ const FList = ({
         keyExtractor={item => item[keyId]}
         data={data}
         renderItem={renderItem}
-        ListEmptyComponent={ListEmptyComponent}
+        ListEmptyComponent={isSuccess && ListEmptyComponent}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         onRefresh={refetchByUser}
         refreshing={isRefetchingByUser}
         ListHeaderComponent={renderHeader}
-        ListFooterComponent={hasNextPage && renderFooter}
+        ListFooterComponent={ListFooterComponent}
         contentContainerStyle={[
           useSafeArea && { paddingBottom: bottom + Spacings.md },
           styles.container,
           contentContainerStyle
         ]}
+        maxToRenderPerBatch={windowSize}
+        windowSize={windowSize}
         {...props}
       />
     </AfterInteraction>
